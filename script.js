d536e5646fb018b23e79dbe90c6c0b08f9ebe57a
Hailegiorgis Wagaye <147624843+HGiorgis@users.noreply.github.com>
2023-11-21 06:57:15 -0800

    Js Day 1
    6 to 10 question

---
+++ script.js
@@ -0,0 +1,36 @@
+
+//Q6
+
+let var1;
+let var2;
+let var3;
+let var4;
+let var5;
+
+//Q7
+
+let num = 3;
+let vstr = "Sting";
+let bool = true;
+let obj = null;
+let char = "a";
+
+//Q8
+
+let firstName = "fname";
+let lastName = "lname";
+let maritalStatus = "Single";
+let country = "Ethiopia";
+let age = 83;
+
+//Q9
+
+let fName = "fname", lName = "lname", marital_Status = "Single", nation = "Ethiopia", $age = 83;
+
+//Q10
+
+let myAge = 25;
+let yourAge = 30;
+
+console.log("I am " + myAge + " years old.");
+console.log("Your age " + yourAge + " years old.");
