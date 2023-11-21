3044b2a81e41d3037a587a9fd90101b1abf52ab3
Hailegiorgis Wagaye <147624843+HGiorgis@users.noreply.github.com>
2023-11-21 14:04:15 -0800

    Update Day 2.js
    line 38: updated from manual string slice to well defined

--- Day 2.js
+++ Day 2.js
@@ -35,8 +35,7 @@ console.log("1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125");
 //Q12
 let str = 'You cannot end a sentence with\
 because because because is a conjunction'
-
- console.log(str.substr(30, 23))
+console.log(str.substr(str.indexOf('because'), ('because '.length)*3))
 
 
 
